a8d93e218c11756b962d3b456a0ac0d91d2429fa
mzerhusen <zerhusma@mail.uc.edu>
2025-03-31 18:49:47 -0400

    Initial commit

---
+++ src/Listmaker.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Listmaker
+{
+    public static void main(String[] args)
+    {
+
+        ArrayList<String> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+    }
+}
